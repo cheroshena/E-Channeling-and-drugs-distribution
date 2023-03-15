df20500b2fa4c5a61a74dca70022c66361c360cb
cheroshena <fwpcheroshena@gmail.com>
2023-03-15 19:12:11 +0530

    Brand crud operation

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
 const categoryRouter = require("./routes/prodcategoryRoute");
 const blogcategoryRouter = require("./routes/blogCatRoute");
+const brandRouter = require("./routes/brandRoute");
 const morgan = require("morgan");
 
 
@@ -26,6 +27,7 @@ app.use("/api/product",productRouter);
 app.use("/api/blog",blogRouter);
 app.use("/api/category",categoryRouter);
 app.use("/api/blogcategory",blogcategoryRouter);
+app.use("/api/brand",brandRouter);
 
 app.use(notFound);
 app.use(errorHandler);
