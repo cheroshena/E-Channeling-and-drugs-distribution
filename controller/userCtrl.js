cdd3bd40dec24a662372ab861259bc7ec7d6c0f3
cheroshena <fwpcheroshena@gmail.com>
2023-04-20 11:43:50 +0530

    update

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -349,37 +349,18 @@ const getWishlist = asyncHandler(async (req, res) => {
 
 //User Add to Cart
 const userCart = asyncHandler(async (req, res) => {
-    const { cart } = req.body;
+    const { productId,quantity,price } = req.body;
 
     const { _id } = req.user;
 
     validateMongoDbId(_id);
     try {
-        let products = []
-        const user = await User.findById(_id);
-        //check if user already have product in cart
-        const alreadyExistCart = await Cart.findOne({ orderby: user._id });
-        if (alreadyExistCart) {
-            alreadyExistCart.remove();
-        }
-        for (let i = 0; i < cart.length; i++) {
-            let object = {};
-            object.product = cart[i]._id;
-            object.count = cart[i].count;
-            object.color = cart[i].color;
-            let getPrice = await Product.findById(cart[i]._id).select("price").exec();
-            object.price = getPrice.price;
-            products.push(object);
-        }
-        let cartTotal = 0;
-        for (let i = 0; i < products.length; i++) {
-            cartTotal = cartTotal + products[i].price * products[i].count;
-        }
 
         let newCart = await new Cart({
-            products,
-            cartTotal,
-            orderby: user?._id,
+            userId:_id,
+            productId,
+            price,
+            quantity
         }).save();
         res.json(newCart);
     }
@@ -393,7 +374,7 @@ const getUserCart = asyncHandler(async (req, res) => {
     const { _id } = req.user;
     validateMongoDbId(_id);
     try {
-        const cart = await Cart.findOne({ orderby: _id }).populate("products.product");
+        const cart = await Cart.find({ userId: _id }).populate("productId");
         res.json(cart);
     } catch (error) {
         throw new Error(error);
--- models/cartModel.js
+++ models/cartModel.js
@@ -1,27 +1,32 @@
 const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var cartSchema = new mongoose.Schema({
-    products: [
-        {
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref:"Product",
+var cartSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
         },
-        count:Number,
-        color:String,
-        price: Number,
+        productId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Product"
         },
-    ],
-    cartTotal:Number,
-    totalAfterDiscount: Number,
-    orderby:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"User",
+        quantity:{
+            type: Number,
+            required:true
+        },
+        price:{
+            type:Number,
+            required:true
+        },
+        
+
+        
+    },
+    {
+        timestamps: true,
     }
-},{
-    timestamps:true,
-});
+);
 
 //Export the model
 module.exports = mongoose.model('Cart', cartSchema);
